14dec380521600cf40cba78bc0ce5792a86de12a
tangtanglove <dai_hang_love@126.com>
2023-02-22 17:36:06 +0800

    chore: wip

---
+++ cmd/portal/idl/post.thrift
@@ -0,0 +1,18 @@
+namespace go post
+
+struct PostRequest {
+  1: string query
+  2: i32 page
+  3: i32 pageSize
+  4: string sorter
+}
+
+struct PostResponse {
+  1: string respBody
+}
+
+// 文章
+service Post {
+    // 获取文章列表
+    PostResponse listHandle(1: PostRequest req)
+}
