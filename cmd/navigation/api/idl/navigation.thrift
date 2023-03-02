f38abe877896590324b24bd4bee714eeea745359
tangtanglove <dai_hang_love@126.com>
2023-03-02 17:37:28 +0800

    fix: wip

--- cmd/navigation/api/idl/navigation.thrift
+++ cmd/navigation/api/idl/navigation.thrift
@@ -1,23 +1,12 @@
-namespace go categoryapi
+namespace go navigationapi
 
-struct GetCategoryListRequest {
-    1: optional string search (api.query="search", api.vd="len($) >= 0")
-    2: i64 page (api.query="page", api.vd="len($) >= 0")
-    3: i64 page_size (api.query="page_size", api.vd="len($) >= 0")
-    4: i64 pid (api.query="pid", api.vd="len($) >= 0")
-    5: string type (api.query="type", api.vd="len($) >= 0")
+struct GetNavigationListRequest {
+    1: i64 limit (api.query="limit", api.vd="len($) >= 0")
+    2: i64 pid (api.query="pid", api.vd="len($) >= 0")
 }
 
-struct GetCategoryListResponse {}
-
-struct GetCategoryDetailRequest {
-    1: i64 id (api.query="id", api.vd="len($) >= 0")
-    2: string name (api.query="name", api.vd="len($) >= 0")
-}
-
-struct GetCategoryDetailResponse {}
+struct GetNavigationListResponse {}
 
 service ApiService {
-    GetCategoryListResponse GetCategoryList(1: GetCategoryListRequest req) (api.get="/api/v1/category/list")
-    GetCategoryDetailResponse GetCategoryDetail(1: GetCategoryDetailRequest req) (api.get="/api/v1/category/detail")
+    GetNavigationListResponse GetCategoryList(1: GetNavigationListRequest req) (api.get="/api/v1/navigation/list")
 }
--- cmd/navigation/rpc/idl/navigation.thrift
+++ cmd/navigation/rpc/idl/navigation.thrift
@@ -1,52 +1,29 @@
-namespace go category
+namespace go navigation
 
-struct Category {
+struct Navigation {
     1: i64 id (vt.gt = "0")
     2: i64 pid
     3: string title
-    4: string name
-    5: string description
+    4: i32 url_type
+    5: string url
     6: string cover_path
     7: string created_at
     8: string updated_at
 }
 
-struct CategoryDetailRequest {
-    1: i64 id (vt.gt = "0")
-    2: optional string name
+struct NavigationListRequest {
+    1: i64 limit (vt.ge = "0")
+    2: string order
+    3: i64 pid (vt.ge = "0")
 }
 
-struct CategoryDetailResponse {
-    1: i64 id (vt.gt = "0")
-    2: i64 pid
-    3: string title
-    4: string name
-    5: string description
-    6: string cover_path
-    7: string created_at
-    8: string updated_at
+struct NavigationListResponse {
+    1: list<Navigation> items
 }
 
-struct CategoryListRequest {
-    1: optional string search
-    2: i64 page (vt.ge = "1")
-    3: i64 page_size (vt.ge = "0")
-    4: string order
-    5: i64 pid (vt.ge = "0")
-    6: string type
-}
-
-struct CategoryListResponse {
-    1: list<Category> items
-    2: i64 total
-}
-
-// 通用分类服务
-service CategoryService {
-  
-    // 获取详情
-    CategoryDetailResponse GetCategoryDetail(1: CategoryDetailRequest req)
+// 导航服务
+service NavigationService {
 
     // 获取列表
-    CategoryListResponse GetCategoryList(1: CategoryListRequest req)
+    NavigationListResponse GetNavigationList(1: NavigationListRequest req)
 }
